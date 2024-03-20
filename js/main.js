2cdb9daa1ee26359c95cc71e76d02ee2cf8694b8
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2024-03-20 20:10:43 +0000

    sliding

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-function exiting(url) {
+document.addEventListener('DOMContentLoaded', function() {
+    var direction = window.location.href.split('/').pop().split('?')[1].split('=')[1];
+    
+    var mainElement = document.querySelector('main');
+    if (direction === '-1') mainElement.style.animationName = 'slide-in-left';
+    else mainElement.style.animationName = 'slide-in-right';
+    mainElement.style.animationDuration = '0.3s';
+    mainElement.style.visibility = 'visible';
+});
+
+function exiting(url, direction) {
     var mainElement = document.querySelector('main');
 
-    mainElement.style.animationName = 'slide-out';
+    if (direction === -1) mainElement.style.animationName = 'slide-out-left';
+    else mainElement.style.animationName = 'slide-out-right';
     mainElement.style.animationDuration = '0.3s';
 
     mainElement.addEventListener('animationend', function () {
         mainElement.style.display = 'none';
-        window.location.href = url;
+        window.location.href = url + '?d=' + direction;
     });
 }
