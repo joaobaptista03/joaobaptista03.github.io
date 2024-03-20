968431ca8df41bca5c869189a05ec63bac451071
joaobaptista03 <joaopedromotabaptista2003@gmail.com>
2024-03-20 19:32:10 +0000

    Update animation duration in exiting function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 function exiting(url) {
     var mainElement = document.querySelector('main');
-    mainElement.style.animationName = 'slide-out';
-    mainElement.style.animationDuration = '0.5s';
 
-    setTimeout(function() {
+    mainElement.style.animationName = 'slide-out';
+    mainElement.style.animationDuration = '0.3s';
+
+    mainElement.addEventListener('animationend', function () {
+        mainElement.style.display = 'none';
         window.location.href = url;
-    }, 300);
+    });
 }
